4fb2efee99d0207324a972b5e0e1c4cc37cd3342
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-01-13 01:40:14 +0530

    html and JS part done

---
+++ script.js
@@ -0,0 +1,18 @@
+function rollDice() {
+
+    const num = document.getElementById('num').value;
+    const result = document.getElementById('result');
+    const image = document.getElementById('image');
+
+    let values = [];
+    let images = [];
+
+    for (let i = 1; i <= num; i++) {
+        let dice = Math.floor(Math.random() * 6) + 1;
+        values.push(dice);
+        images.push(`<img src="dice-images/dice${dice}.png" alt="dice${dice}">`);
+    }
+
+    result.textContent = `dice: ${values.join(', ')}`;
+    image.innerHTML = `${images.join(' ')}`;
+}
